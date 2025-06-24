cf19b250114cdfd3f66fecc6f4f4b4cb03e70531
Mohith sai b <bmstpt1@gmail.com>
2025-06-24 09:38:04 +0530

    Add files via upload

---
+++ oops.java
@@ -0,0 +1,13 @@
+class Student_Details{
+    string name;
+    int year;
+    int percentage;
+    int rollno;
+    int total;
+    Student_Details(name,year,rollno){
+        this.name = name;
+        this.year = year;
+        this.rollno = rollno;
+        
+    }
+}
